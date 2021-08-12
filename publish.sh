dcb1fb11aca37f4bc0f580f7acadb48909cb8fff
qihuan <qihuan92@126.com>
2021-08-12 17:51:15 +0800

    修改上传配置

--- publish.sh
+++ publish.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+export JAVA_HOME=/Applications/Android\ Studio.app/Contents/jre/Contents/Home
+export PATH=$JAVA_HOME/bin:$PATH
+
 ./gradlew annotation:clean annotation:assemble annotation:generatePomFileForReleasePublication annotation:publishReleasePublicationToOSSRHRepository
 ./gradlew compiler:clean compiler:assemble compiler:generatePomFileForReleasePublication compiler:publishReleasePublicationToOSSRHRepository
 ./gradlew runtime:clean runtime:assembleRelease runtime:generatePomFileForReleasePublication runtime:publishReleasePublicationToOSSRHRepository
