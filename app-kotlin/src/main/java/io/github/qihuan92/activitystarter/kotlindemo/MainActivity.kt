f75282262e94a65d2ec01e4eae0affe15a536bf6
qihuan <qihuan92@126.com>
2022-09-09 10:28:32 +0800

    fix: No providers found in processor classpath.

--- app-kotlin/src/main/java/io/github/qihuan92/activitystarter/kotlindemo/MainActivity.kt
+++ app-kotlin/src/main/java/io/github/qihuan92/activitystarter/kotlindemo/MainActivity.kt
@@ -2,7 +2,9 @@ package io.github.qihuan92.activitystarter.kotlindemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import io.github.qihuan92.activitystarter.annotation.Builder
 
+@Builder
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
